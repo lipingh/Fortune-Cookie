107f1415a7b9497044eabdb886cbf73602b8c7c9
lipingh <lipingh@andrew.cmu.edu>
2021-08-25 09:20:17 -0700

    add trash icon to remove messages

--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const MessageList = ({ cookies }) => (
-  <div className="cookie-message-list">
-    <div className="message-title">&#129376; My Favorite Fortune Cookie Messages</div>
-    <div className="cookie-message-list-container">
-      {cookies.map((cookie, index) => (
-        <div key={index}>
-          &#129376;
-          {cookie.message}
-        </div>
-      ))}
+const MessageList = ({ cookies }) => {
+  const handleDeleteMessage = () => {
+
+  };
+  return (
+    <div className="cookie-message-list">
+      <div className="message-title">&#129376; My Favorite Fortune Cookie Messages</div>
+      <div className="cookie-message-list-container">
+        {cookies.map((cookie, index) => (
+          <div key={index}>
+            &#129376;
+            {cookie.message}
+            <span className="dislike" onClick={() => handleDeleteMessage(cookie)}>&#128465;</span>
+          </div>
+
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
 // MessageList.propTypes = {
 //   cookies: PropTypes.arrayOf({
 //     _id: PropTypes.string,
